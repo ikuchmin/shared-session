1253d3e515a17201ebfcb61871379e3d572185c8
Ilya Kuchmin <kuchmin@haulmont.com>
2020-05-11 11:26:55 +0400

    Initial project commit. There is just scaffolding

---
+++ modules/portal/src/ru/udya/sharedsession/portal/controllers/LoginController.java
@@ -0,0 +1,20 @@
+package ru.udya.sharedsession.portal.controllers;
+
+import ru.udya.sharedsession.portal.command.LoginUserCommand;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+public class LoginController {
+
+    @RequestMapping(value = "login", method = RequestMethod.GET)
+    public String login(Model model) {
+        final LoginUserCommand loginUserCommand = new LoginUserCommand();
+        model.addAttribute(loginUserCommand);
+
+        return "login";
+    }
+}
