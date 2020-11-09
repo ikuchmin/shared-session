4e033320669913c6fa185100866d616185c0bb6a
Ilya Kuchmin <kuchmin@haulmont.com>
2020-11-09 13:46:56 +0400

    Make SharedUserSessionId as Serializable

--- modules/global/src/ru/udya/sharedsession/domain/SharedUserSessionId.java
+++ modules/global/src/ru/udya/sharedsession/domain/SharedUserSessionId.java
@@ -2,7 +2,7 @@ package ru.udya.sharedsession.domain;
 
 import java.io.Serializable;
 
-public interface SharedUserSessionId<ID extends Serializable> {
+public interface SharedUserSessionId<ID extends Serializable> extends Serializable {
 
     ID getSharedId();
 
